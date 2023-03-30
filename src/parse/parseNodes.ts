a4563a3e494bd15159ad464466b67fa227b6ba8b
Xie Yuheng <xyheme@gmail.com>
2023-03-31 07:44:02 +0800

    use "markdown-it-footnote" -- early for catching bug

--- src/parse/parseNodes.ts
+++ src/parse/parseNodes.ts
@@ -1,9 +1,12 @@
 import MarkdownIt from "markdown-it"
+import FootnotePlugin from "markdown-it-footnote"
 import { Node } from "../node"
 import { Token } from "./Token"
 import { runTokens } from "./runTokens"
 
 const parser = new MarkdownIt({ html: false })
+
+parser.use(FootnotePlugin)
 
 export function parseNodes(text: string): Array<Node> {
   const tokens: Array<Token> = parser.parse(text, {})

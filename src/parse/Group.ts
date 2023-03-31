41f4f1be4548a8cee5d66c2fb31fcae9da91dfd1
Xie Yuheng <xyheme@gmail.com>
2023-04-01 07:04:18 +0800

    Revert "extract `Group`"
    This reverts commit ec1545c064f3942bf507e8004a1b86b1ef3b37eb.

--- src/parse/Group.ts
+++
@@ -1,5 +0,0 @@
-import { Node } from "../node"
-
-export type Group =
-  | { kind: "Finial"; nodes: Array<Node> }
-  | { kind: "Reparse"; nodes: Array<Node> }
--- src/parse/parseDocument.ts
+++ src/parse/parseDocument.ts
@@ -1,14 +1,13 @@
+import { Node } from "../node"
 import * as Nodes from "../nodes"
 import { parseDocument as parseDocumentWithoutHTML } from "../parse-without-html"
-import { grouping } from "./grouping"
 import { reparseNodes } from "./reparseNodes"
 
 export function parseDocument(text: string): Nodes.Document {
   const document = parseDocumentWithoutHTML(text)
 
-  const groups = grouping(document.children)
-
-  const reparsedGroups = groups.map((group) => {
+  // Group the top level nodes, some for further XML parsing.
+  const groups = grouping(document.children).map((group) => {
     if (group.kind === "Reparse") {
       const text = group.nodes.map((node) => node.format()).join("\n\n")
       return { kind: "Reparse", nodes: reparseNodes(text) }
@@ -17,7 +16,43 @@ export function parseDocument(text: string): Nodes.Document {
     return group
   })
 
-  document.children = reparsedGroups.flatMap((group) => group.nodes)
+  document.children = groups.flatMap((group) => group.nodes)
 
   return document
 }
+
+function grouping(nodes: Array<Node>): Array<Group> {
+  const groups: Array<Group> = []
+
+  for (const node of nodes) {
+    if (node instanceof Nodes.CodeBlock) {
+      const group = groups.pop()
+      if (group === undefined) {
+        groups.push({ kind: "Finial", nodes: [node] })
+      } else if (group.kind === "Finial") {
+        group.nodes.push(node)
+        groups.push(group)
+      } else {
+        groups.push(group)
+        groups.push({ kind: "Finial", nodes: [node] })
+      }
+    } else {
+      const group = groups.pop()
+      if (group === undefined) {
+        groups.push({ kind: "Reparse", nodes: [node] })
+      } else if (group.kind === "Reparse") {
+        group.nodes.push(node)
+        groups.push(group)
+      } else {
+        groups.push(group)
+        groups.push({ kind: "Reparse", nodes: [node] })
+      }
+    }
+  }
+
+  return groups
+}
+
+type Group =
+  | { kind: "Finial"; nodes: Array<Node> }
+  | { kind: "Reparse"; nodes: Array<Node> }

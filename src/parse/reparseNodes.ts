1089e2b74cdad030a9c86c485427db329e830fd9
Xie Yuheng <xyheme@gmail.com>
2023-04-01 11:54:08 +0800

    up

--- src/parse/reparseNodes.ts
+++ src/parse/reparseNodes.ts
@@ -9,23 +9,25 @@ export function reparseNodes(nodes: Array<Node>): Array<Node> {
   const groups = groupingNodesForReparse(nodes).map((group) => {
     if (group.kind === "Reparse") {
       const text = group.nodes.map((node) => node.format()).join("\n\n")
-      return { kind: "Reparse", nodes: reparse(text) }
+      const groups = groupingXNodes(parse(text))
+
+      if (!groups.find((group) => group.kind === "Element")) {
+        return group
+      }
+
+      const nodes = groups.flatMap((group) => {
+        if (group.kind === "Text") {
+          return parseNodesWithoutHTML(group.text)
+        } else {
+          return [new Nodes.Element({ element: group.element })]
+        }
+      })
+
+      return { kind: "Reparse", nodes }
     }
 
     return group
   })
 
   return groups.flatMap((group) => group.nodes)
-}
-
-function reparse(text: string): Array<Node> {
-  const groups = groupingXNodes(parse(text))
-
-  return groups.flatMap((group) => {
-    if (group.kind === "Text") {
-      return parseNodesWithoutHTML(group.text)
-    } else {
-      return [new Nodes.Element({ element: group.element })]
-    }
-  })
 }

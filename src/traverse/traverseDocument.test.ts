66e67343ecd49dc8225d0fdaafca380534658421
Xie Yuheng <xyheme@gmail.com>
2023-10-16 17:31:26 +0800

    up

---
+++ src/traverse/traverseDocument.test.ts
@@ -0,0 +1,25 @@
+import { test } from "vitest"
+import { parseDocument } from "../parse"
+import { traverseDocument } from "./traverseDocument"
+
+test("traverseDocument", () => {
+  const text = `\
+---
+title: Hello world
+authors: [xieyuheng, yuhengxie, hengxieyu]
+date: 2021-09-22
+---
+
+# Hiya
+
+Hi Hi Yo Yo
+
+![example image](https://example.com "example title")
+`
+
+  const document = parseDocument(text)
+
+  for (const node of traverseDocument(document)) {
+    console.log(node)
+  }
+})
